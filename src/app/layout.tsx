835cf2551fe471d3fdd53d2785551cf67ee8e3ff
Guckert <110573597+Guckert@users.noreply.github.com>
2025-11-19 13:58:59 +1300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export const metadata: Metadata = {
 };
 
 const GA_MEASUREMENT_ID = 'G-FXCH0GL6V4';
+const META_PIXEL_ID = '731994975939671';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   // Single JSON-LD graph (Organization + LocalBusiness + WebSite)
@@ -170,6 +171,33 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+
+        {/* Meta Pixel base code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !(function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Noscript fallback for pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
 
         <Header />
         {children}
